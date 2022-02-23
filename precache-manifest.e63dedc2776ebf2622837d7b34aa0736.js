c85f0b71c0e5b16a49f0ff505232fc75d6404ee0
Bulat Constantin <bulatconstantin@192-168-0-100.rdsnet.ro>
2022-02-23 09:22:31 +0200

    Updates

---
+++ precache-manifest.e63dedc2776ebf2622837d7b34aa0736.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81d1701fde3a5f2f811b5fa6fee62467",
+    "url": "/react-and-morty/index.html"
+  },
+  {
+    "revision": "db4ab85dfde27ecdf670",
+    "url": "/react-and-morty/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "b3e194e8aee24d555095",
+    "url": "/react-and-morty/static/css/main.35b1ab03.chunk.css"
+  },
+  {
+    "revision": "db4ab85dfde27ecdf670",
+    "url": "/react-and-morty/static/js/2.4469d9d0.chunk.js"
+  },
+  {
+    "revision": "ae6bb62977b25ec73f0509e3cbccba19",
+    "url": "/react-and-morty/static/js/2.4469d9d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3e194e8aee24d555095",
+    "url": "/react-and-morty/static/js/main.6b8e7ef1.chunk.js"
+  },
+  {
+    "revision": "453b88591806e02178dd",
+    "url": "/react-and-morty/static/js/runtime-main.726c9020.js"
+  }
+]);
